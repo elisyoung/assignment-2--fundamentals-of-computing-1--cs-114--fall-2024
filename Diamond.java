496c0594b24ee179823fb704528cf273d47d1c0d
elisyoung <eyoung@hartford.edu>
2024-10-09 16:35:10 -0400

    Took input and found if even or odd

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    int userInput;
+    boolean valueIsOdd;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a number:");
+    userInput = input.nextInt();
+
+    if(userInput % 2 == 1){
+      valueIsOdd = true;
+    } else {
+      valueIsOdd = false;
+    }
+
+    System.out.println(valueIsOdd);
+
+
   }
 }

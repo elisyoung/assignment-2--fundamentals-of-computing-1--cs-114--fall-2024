73b2d87936a9c4e9cae2a815794eec636fc77d3b
elisyoung <eyoung@hartford.edu>
2024-10-12 16:59:37 -0400

    printed top half of even diamond

--- Diamond.java
+++ Diamond.java
@@ -48,12 +48,36 @@ public class Diamond {
         };
 
         System.out.println();
-
       }
-
 
     } else {
       System.out.println("this value is even");
+      //print the first asterisk of even diamond
+      for(int spacesInTheFirstLine = userInput - 1; spacesInTheFirstLine > 0; spacesInTheFirstLine--){
+        System.out.print(" ");
+      }
+      System.out.println("*");
+
+      //print top half of even diamond
+      for(int currentRow = 1; currentRow <= userInput / 2; currentRow++){
+
+        int spacesInThisRow = ((userInput / 2) - currentRow) * 2;
+        int asterisksInThisRow = currentRow * 2;
+
+        for(int spaces = 1; spaces <= spacesInThisRow; spaces++){
+          System.out.print(" ");
+        }
+
+        for(int asterisks = 1; asterisks <= asterisksInThisRow; asterisks++){
+          System.out.print("* ");
+        }
+
+        System.out.println();
+      }
+
+      //print bottom half of even diamond
+
+      //print the last asterisk of even diamond
     }
 
 
